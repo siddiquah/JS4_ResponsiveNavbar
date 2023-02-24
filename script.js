75e85828b4aa9b22c1c8a51dab744addd29211bb
siddiquah anjum <50186478+siddiquah@users.noreply.github.com>
2023-02-24 23:36:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// const burger = document.querySelector('.burger');
+//       const nav = document.querySelector('.nav-links');
+//       const navLinks = document.querySelectorAll('.nav-links li');
+
+//       burger.addEventListener('click', () => {
+//         nav.classList.toggle('active');
+//         navLinks.forEach((link, index) => {
+//         });
+//         burger.classList.toggle('active');
+//       });
+
+
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.nav-links');
+let navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle()
+});
